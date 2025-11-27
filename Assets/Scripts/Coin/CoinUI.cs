4892fd89f74e6265b1142ce1b0fc27207eb95217
yoogun12 <yoogun1313@gmail.com>
2025-11-27 23:33:37 +0900

    [Feat] 코인 데이터 아이템 , 적 죽이면 코인 드랍

---
+++ Assets/Scripts/Coin/CoinUI.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinUI : MonoBehaviour
+{
+    public Text coinText;
+
+    void Update()
+    {
+        coinText.text = CoinManager.Instance.coin.ToString();
+    }
+}
