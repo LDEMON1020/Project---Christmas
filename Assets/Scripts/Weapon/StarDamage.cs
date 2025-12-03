0943e846e7b2a612e71e6b849a30a759362ad41f
yoogun12 <yoogun1313@gmail.com>
2025-12-04 01:38:19 +0900

    [Feat] 선물상자 아이템화, 코드 추가와 수정

---
+++ Assets/Scripts/Weapon/StarDamage.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarDamage : MonoBehaviour
+{
+    public int damage = 3;
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            ReconEnemyController recon = collision.GetComponent<ReconEnemyController>();
+            if (recon != null)
+            {
+                recon.TakeDamage(damage);
+            }
+            EnemyController enemy = collision.GetComponent<EnemyController>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+
+            RangedEnemy ranged = collision.GetComponent<RangedEnemy>();
+            if (ranged != null)
+            {
+                ranged.TakeDamage(damage);
+            }
+        }
+    }
+
+
+}
